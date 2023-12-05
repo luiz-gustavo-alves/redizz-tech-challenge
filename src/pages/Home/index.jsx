77351281a6a630073eb53bbfdacdbb3d3ea296d4
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-12-05 19:19:41 -0300

    feat: undo and show circle buttons

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Wrapper, Content } from "./style";
+import { Wrapper, Content, ButtonContainer, Button } from "./style";
 
 export default function Home() {
   const [coords, setCoords] = useState(null);
@@ -14,11 +14,30 @@ export default function Home() {
     }
   };
 
+  const handleUndoCircle = (e) => {
+    e.stopPropagation();
+    if (coords != null) {
+      setDisableClick(true);
+    }
+  }
+
+  const handleShowCircle = (e) => {
+    e.stopPropagation();
+    if (coords != null) {
+      setDisableClick(false);
+    }
+  }
+
+  const showCircle = (coords != null && !disableClick);
   return (
     <Wrapper onClick={getScreenCoords}>
-      {coords != null &&
-        <Content $coords={coords} $disableClick={disableClick} />
+      {showCircle &&
+        <Content $coords={coords} />
       }
+      <ButtonContainer>
+        <Button onClick={handleUndoCircle}>Desfazer</Button>
+        <Button onClick={handleShowCircle}>Refazer</Button>
+      </ButtonContainer>
     </Wrapper>
   )
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -15,3 +15,27 @@ export const Content = styled.div`
   left: ${(props) => `${props.$coords.x - 25}px`};
   top: ${(props) => `${props.$coords.y - 25}px`};
 `;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-around;
+  position: absolute;
+  bottom: 80px;
+  left: 0;
+`;
+
+export const Button = styled.button`
+  width: 125px;
+  height: 80px;
+  font-size: 22px;
+  color: #FFF;
+  border: none;
+  background-color: blue;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
