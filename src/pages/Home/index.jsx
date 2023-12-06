8c3bba0194b0b9a508575764904ad1c6a4bf10ab
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-12-05 23:53:29 -0300

    chore: removed unused dependencies

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Wrapper, ButtonContainer, Button } from "./style";
 import { Circle } from "../../components";
 
